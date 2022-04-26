46b7a7d79fbf90b68c2cd473876c866b6d6c8f55
neilpreyes <98624021+neilpreyes@users.noreply.github.com>
2022-04-25 18:48:04 -0700

    Added Constructor
    Implemented private variables

--- Graph.java
+++ Graph.java
@@ -2,5 +2,8 @@ public class Graph<E> {
     private boolean[][] edges; // edges[i][j] is true if there is a vertex from i to j
     private E[] labels; // labels[i] contains the label for vertex i
 
-    
+    public Graph(int n) {
+        edges = new boolean[n][n]; //all values initially false
+        labels = (E[]) new Object[n]; //All values initially null
+    }
 }
