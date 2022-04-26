fb3b954f9044efc11660db8143d801904cbf480c
neilpreyes <98624021+neilpreyes@users.noreply.github.com>
2022-04-25 18:46:18 -0700

    Updated graph.java for initial code

--- Graph.java
+++ Graph.java
@@ -1,3 +1,6 @@
 public class Graph<E> {
+    private boolean[][] edges; // edges[i][j] is true if there is a vertex from i to j
+    private E[] labels; // labels[i] contains the label for vertex i
+
     
 }
