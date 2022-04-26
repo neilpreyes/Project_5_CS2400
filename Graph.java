5cb1f2f5f040c1cd616a8127a78705f00d9a63e3
neilpreyes <98624021+neilpreyes@users.noreply.github.com>
2022-04-25 18:51:15 -0700

    Added accessor methods
    getLabel, isEdge, addEdge

--- Graph.java
+++ Graph.java
@@ -6,4 +6,18 @@ public class Graph<E> {
         edges = new boolean[n][n]; //all values initially false
         labels = (E[]) new Object[n]; //All values initially null
     }
+
+    //Accessor method to get the label of a vertex of this Graph
+    public E getLabel(int vertex){
+        return labels[vertex];
+    }
+
+    //Test whether an edge exists
+    public boolean isEdge(int source, int target){
+        return edges[source][target];
+    }
+
+    public void addEdge(int source, int target){
+        edges[source][target] = true;
+    }
 }
