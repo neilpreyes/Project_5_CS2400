ff1a93f3410276a27e217aa57a84009d6f093ee4
neilpreyes <98624021+neilpreyes@users.noreply.github.com>
2022-05-09 12:25:14 -0700

    Update ListIteratorInterface.java

--- ListADT/ListIteratorInterface.java
+++ ListADT/ListIteratorInterface.java
@@ -1,13 +1,7 @@
 package ListADT;
 
 import java.util.Iterator;
-/**
-   An interface for the ADT list that has an iterator.
-   
-   @author Frank M. Carrano
-   @author Timothy M. Henry
-   @version 5.0
-*/
+
 public interface ListIteratorInterface<T> extends ListInterface<T>, Iterable<T>
 {
    public Iterator<T> getIterator();
